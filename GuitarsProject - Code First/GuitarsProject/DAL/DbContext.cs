36824a96b77f041fed25f48f2cc08117db8243aa
Alexey_k <Alexey_k@fast-report.com>
2018-02-07 14:23:14 +0300

    added Code first mssql example

--- GuitarsProject - Code First/GuitarsProject/DAL/DbContext.cs
+++ GuitarsProject - Code First/GuitarsProject/DAL/DbContext.cs
@@ -9,5 +9,9 @@ namespace GuitarsProject
         { }
 
         public DbSet<Guitar> Guitars { get; set; }
+        public DbSet<Body> Bodies { get; set; }
+        public DbSet<GuitarString> GuitarStrings { get; set; }
+        public DbSet<Neck> Necks { get; set; }
+        public DbSet<Pickup> Pickups { get; set; }
     }
 }
